90d2b457fbeeade6194bd2e4be4a6954ba0856a1
Dmitrii K <kushnerdmitry@yandex.ru>
2019-07-23 22:53:07 +0300

    added sceleton of api requester

---
+++ ruby/ApiRequester.rb
@@ -0,0 +1,10 @@
+# require 'net/http'
+
+# class ApiRequester 
+#   def get_name_of_??????? param1, params2 ......
+#     Net:HTTP.post/get_form(
+#       URI("uri"),
+#       param: param1,,,,
+#     )
+#   end
+# end
