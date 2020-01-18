47d99d8ca7442495ce0b85935c86aeecdbaa46b3
d.kushner <kushnerdmitry@yandex.ru>
2020-01-18 17:15:13 +0300

    added cron script

---
+++ ruby/run_initialize.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# /bin/bash -c 'export PATH="$HOME/.rbenv/bin:$PATH" ; eval "$(rbenv init -)"; cd /root/kislorod/ruby; ./run_initialize.sh >> cronout 2>&1'
+ruby /root/kislorod/ruby/initialize.rb
